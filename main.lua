4f57817e42311972768b4bf8c6f495e9e45fe6d4
NickTheScripterLua <65808377+NickTheScripterLua@users.noreply.github.com>
2021-06-10 20:07:35 +0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-local function a()local b=pcall(function()game:GetService("HttpService"):GetAsync("https://www.google.com/")end)return b end;print(a())
+local function a()local b=pcall(function()game:GetService("HttpService"):GetAsync("https://www.google.com/")end)return b end;print(nill())
